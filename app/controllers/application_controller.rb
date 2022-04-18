77d7d99a21d21646fa4c230809d5c11e578755a4
roeihaviv <rhaviv@chicagobooth.edu>
2022-04-18 14:55:56 +0000

    Lots of progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,28 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
 
     # redirect_to("https://www.wikipedia.org")
     #  render({ :plain => "Howdy, Woerld!"})
     render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+   @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb" })
   end
 end
